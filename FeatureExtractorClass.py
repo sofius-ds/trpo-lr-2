fc02dea713ea92728c4c430a02945ed5a634fdfd
Sofia Kazachek <kazachek2004si@gmail.com>
2026-09-22 23:32:56 +0300

    Changes in branch 2

--- FeatureExtractorClass.py
+++ FeatureExtractorClass.py
@@ -19,5 +19,3 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
             data = data.drop(columns="timestamp")
 
         return data
-
-print("FeatureExtractor class has been defined successfully.")

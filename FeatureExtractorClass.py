e1ff2875046c08ab06714c0b4fb0813e59f70510
Sofia Kazachek <kazachek2004si@gmail.com>
2026-09-23 18:06:00 +0300

    Fixed collisions

--- FeatureExtractorClass.py
+++ FeatureExtractorClass.py
@@ -16,7 +16,6 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
             data["timestamp"] = pd.to_datetime(data["timestamp"])
             data["hour"] = data["timestamp"].dt.hour
             data["dayofweek"] = data["timestamp"].dt.weekday
-            data["is_weekend"] = data["dayofweek"] >= 5
             data = data.drop(columns="timestamp")
 
         return data

a32126cea12e1702e881c41a9bb5a60b781f2e55
Sofia Kazachek <kazachek2004si@gmail.com>
2026-09-23 18:16:56 +0300

    Fixea and added info

--- FeatureExtractorClass.py
+++ FeatureExtractorClass.py
@@ -19,3 +19,5 @@ class FeatureExtractor(BaseEstimator, TransformerMixin):
             data = data.drop(columns="timestamp")
 
         return data
+
+    print("FeatureExtractor initialized and ready to transform data.")
